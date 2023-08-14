d479945c5e4fcd2db6d8f6ab7319aa1e87c0e8c6
Rimantas <belauzas@gmail.com>
2023-08-14 09:26:26 +0300

    User header menu

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../context/UserContext";
 
 export function Header() {
     const ctx = useContext(UserContext);
     const userLoggedIn = ctx.user.loggedIn;
+    const [menuVisibility, setMenuVisibility] = useState(false);
 
     const publicMenu = (
         <div className="col-md-3 text-end">
@@ -15,15 +16,24 @@ export function Header() {
 
     const userMenu = (
         <div className="col-md-3 dropdown text-end">
-            <Link to="/" className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            <div onClick={() => setMenuVisibility(!menuVisibility)} className="d-block link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
                 <img src="https://github.com/mdo.png" alt="mdo" width="32" height="32" className="rounded-circle" />
-            </Link>
-            <ul className="dropdown-menu text-small">
-                <li><Link className="dropdown-item" to="/">New project...</Link></li>
-                <li><Link className="dropdown-item" to="/">Settings</Link></li>
-                <li><Link className="dropdown-item" to="/">Profile</Link></li>
+            </div>
+            <ul className={`dropdown-menu text-small ${menuVisibility ? 'show' : ''}`}
+                style={{
+                    position: 'absolute',
+                    inset: '0px 0px auto auto',
+                    margin: '0px',
+                    transform: 'translate3d(0px, 34.4px, 0px)',
+                }}>
+                <li><Link className="dropdown-item" to="/dashboard">Dashboard</Link></li>
+                <li><Link className="dropdown-item" to="/movies">Movies</Link></li>
+                <li><Link className="dropdown-item" to="/movies/add">New movie</Link></li>
                 <li><hr className="dropdown-divider" /></li>
-                <li><Link className="dropdown-item" to="/">Sign out</Link></li>
+                <li><Link className="dropdown-item" to="/settings">Settings</Link></li>
+                <li><Link className="dropdown-item" to="/profile">Profile</Link></li>
+                <li><hr className="dropdown-divider" /></li>
+                <li><Link className="dropdown-item" to="/logout">Sign out</Link></li>
             </ul>
         </div>
     );
